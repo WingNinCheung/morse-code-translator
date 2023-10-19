d454e4b35971e9bcb47c97c6d1cf0a6d5f089f2b
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2023-10-19 16:57:01 -0700

    completed frontend to convert a letter to morse code from user

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import "./App.css";
 
 function App() {
   const [inputString, setInputString] = useState("");
+  const [morseCode, setMorseCode] = useState("");
   const convertString = async () => {
     const data = { text: inputString };
-    const res = await fetch("http://localhost:5001/translate/toMorse/", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    });
-    console.log(await res.json());
+    try {
+      const res = await fetch("http://localhost:5001/translate/toMorse/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      if (res.ok) {
+        const code = await res.json();
+        setMorseCode(code);
+      } else {
+        const error = await res.json();
+        setMorseCode(error.error);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -23,7 +34,7 @@ function App() {
       </div>
       <div className="output">
         <span>Morse code: </span>
-        <span>{inputString}</span>
+        <span>{morseCode}</span>
       </div>
     </div>
   );
